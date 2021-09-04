0e1d40dc5867fb30ffc0a38cc61fe1d3db797ce7
masafumi <masa455.wv@gmail.com>
2021-09-05 01:40:20 +0900

    add:フッターをコンポーネント化

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Header} from '../components/Header'
-
+import {Footer} from '../components/Footer'
 import { Treasure } from '../components/Treasure'
 
 const Home: NextPage = () => {
@@ -40,19 +40,7 @@ const Home: NextPage = () => {
           <Treasure />
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer/>
     </div>
   )
 }
