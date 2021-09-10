2e492cf3baed60231f65cb5b0fb5670f292bdea9
masafumi <masa455.wv@gmail.com>
2021-09-10 17:48:06 +0900

    add:MetaMaskと繋げるボタンをコンポーネント化

--- pages/connectMyWallet.tsx
+++ pages/connectMyWallet.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Image from 'next/image'
 import fate from '../public/fate.png'
+import { ConnectMetaMaskButton } from '../components/ConnectMetaMaskButton'
 
 export default function connectMyWallet() {
   return (
@@ -24,42 +25,7 @@ export default function connectMyWallet() {
         {/* testネットワークに繋ぐ */}
         {/* メタマスクをつないでユーザー認証する */}
         <div className="row">
-          <div className="col-4">
-            <div
-              className="border border-info rounded m-1"
-              style={{ height: 200, width: 250, boxShadow: '0px 0px 6px #ccc' }}
-            ></div>
-          </div>
-          <div className="col-4">
-            <div
-              className="border border-info rounded m-1"
-              style={{ height: 200, width: 250, boxShadow: '0px 0px 6px #ccc' }}
-            ></div>
-          </div>
-          <div className="col-4">
-            <div
-              className="border border-info rounded m-1"
-              style={{ height: 200, width: 250, boxShadow: '0px 0px 6px #ccc' }}
-            ></div>
-          </div>
-          <div className="col-4">
-            <div
-              className="border border-info rounded m-1"
-              style={{ height: 200, width: 250, boxShadow: '0px 0px 6px #ccc' }}
-            ></div>
-          </div>
-          <div className="col-4">
-            <div
-              className="border border-info rounded m-1"
-              style={{ height: 200, width: 250, boxShadow: '0px 0px 6px #ccc' }}
-            ></div>
-          </div>
-          <div className="col-4">
-            <div
-              className="border border-info rounded m-1"
-              style={{ height: 200, width: 250, boxShadow: '0px 0px 6px #ccc' }}
-            ></div>
-          </div>
+          <ConnectMetaMaskButton />
         </div>
         <p>
           We do not own your private keys and cannot access your funds without your confirmation.
