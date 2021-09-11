03ffdc43758f2470ff47fb0fcf00fe117b5bb41c
masafumi <masa455.wv@gmail.com>
2021-09-11 18:21:38 +0900

    add:グローバル変数格納ページを追加し、全てのページで使えるようにした(headerがあるページ全て)

--- pages/createSingleTreasure.tsx
+++ pages/createSingleTreasure.tsx
@@ -6,87 +6,90 @@ import { Header } from '../components/Header'
 import { MainButton } from '../components/MainButton'
 import { Footer } from '../components/Footer'
 import { H6 } from '../components/H6'
+import { AuthProvider } from './context/AuthContext'
 
 export default function createSingleTreasure() {
   return (
-    <div style={{ background: '#0b1118', color: '#eef0e6' }}>
-      <Header />
-      <div className="container my-5 h-100">
-        <div className="row">
-          <div className="col-8">
-            <Link href="/createTreasure">
-              <a>←収集可能なタイプを管理する</a>
-            </Link>
-            <h1>単一の収集品を作成する</h1>
-            {/* ファイルのアップロード */}
-            <div>
-              <h4>ファイルをアップロードする</h4>
-              <div className="border border-info w-100 rounded" style={{ height: 300 }}></div>
-            </div>
-            <div style={{ height: 40 }}></div>
-            {/* 市場に出す */}
-            <div>
-              <h4>市場に出す</h4>
-              <H6 title={'ユーザーがNFTをすぐに購入できるように価格を入力します'} />
-              <div className="row" style={{ justifyContent: 'space-between' }}>
-                {/* コンポーネントをいろんなところで使いまわしているけど、ここだけonclickイベントを入れたい！それはできるのか？
+    <AuthProvider>
+      <div style={{ background: '#0b1118', color: '#eef0e6' }}>
+        <Header />
+        <div className="container my-5 h-100">
+          <div className="row">
+            <div className="col-8">
+              <Link href="/createTreasure">
+                <a>←収集可能なタイプを管理する</a>
+              </Link>
+              <h1>単一の収集品を作成する</h1>
+              {/* ファイルのアップロード */}
+              <div>
+                <h4>ファイルをアップロードする</h4>
+                <div className="border border-info w-100 rounded" style={{ height: 300 }}></div>
+              </div>
+              <div style={{ height: 40 }}></div>
+              {/* 市場に出す */}
+              <div>
+                <h4>市場に出す</h4>
+                <H6 title={'ユーザーがNFTをすぐに購入できるように価格を入力します'} />
+                <div className="row" style={{ justifyContent: 'space-between' }}>
+                  {/* コンポーネントをいろんなところで使いまわしているけど、ここだけonclickイベントを入れたい！それはできるのか？
               コンポーネント側で処理の仕方がわからない
               それかコンポーネントを分けるか */}
-                <MainButton width={250} height={250} title={'1'} />
-                <MainButton width={250} height={250} title={'2'} />
-                <MainButton width={250} height={250} title={'3'} />
+                  <MainButton width={250} height={250} title={'1'} />
+                  <MainButton width={250} height={250} title={'2'} />
+                  <MainButton width={250} height={250} title={'3'} />
+                </div>
               </div>
-            </div>
-            <div style={{ height: 40 }}></div>
-            {/* 価格 */}
-            <h4>価格</h4>
-            <H6 title={'サービス料2.5％'} />
-            <H6 title={'あなたは0ETHを受け取ります'} />
-            {/* 購入したらロックを解除 */}
-            <div>
-              {/* <h4>購入したらロックを解除⇦これの意味がわからない</h4>
+              <div style={{ height: 40 }}></div>
+              {/* 価格 */}
+              <h4>価格</h4>
+              <H6 title={'サービス料2.5％'} />
+              <H6 title={'あなたは0ETHを受け取ります'} />
+              {/* 購入したらロックを解除 */}
+              <div>
+                {/* <h4>購入したらロックを解除⇦これの意味がわからない</h4>
             <H6 title={"トランザクションが成功すると、コンテンツのロックが解除されます"} /> */}
-            </div>
-            {/* コレクションを選択 */}
-            <div>
-              <h4>コレクションを選択</h4>
-              <div className="row">
-                <MainButton width={250} height={250} title={'1'} />
-                <MainButton width={250} height={250} title={'2'} />
               </div>
-            </div>
-            {/* 題名 */}
-            <h4>題名</h4>
-            {/* 説明（オプション） */}
-            <h4>
-              説明
-              <span className="text-white-50" style={{ fontSize: 16 }}>
-                （オプション）
-              </span>
-            </h4>
-            <H6 title={'改行が保持されます'} />
+              {/* コレクションを選択 */}
+              <div>
+                <h4>コレクションを選択</h4>
+                <div className="row">
+                  <MainButton width={250} height={250} title={'1'} />
+                  <MainButton width={250} height={250} title={'2'} />
+                </div>
+              </div>
+              {/* 題名 */}
+              <h4>題名</h4>
+              {/* 説明（オプション） */}
+              <h4>
+                説明
+                <span className="text-white-50" style={{ fontSize: 16 }}>
+                  （オプション）
+                </span>
+              </h4>
+              <H6 title={'改行が保持されます'} />
 
-            {/* 使用料 */}
-            <h4>使用料</h4>
-            <H6 title={'推奨：0％、10％、20％、30％。最大は50％です'} />
-            <Button
-              title={'アイテムを作成する'}
-              message={'アイテムを作成しますか？'}
-              color={'#eef0e6'}
-              backColor={'#3a526f'}
-              width={300}
-            />
-          </div>
-          {/* プレビュー用スペース */}
-          <div className="col-4">
-            <div className="position-fixed">
-              <div className="mt-5">プレビュー用スペース</div>
-              <MainButton width={414} height={500} />
+              {/* 使用料 */}
+              <h4>使用料</h4>
+              <H6 title={'推奨：0％、10％、20％、30％。最大は50％です'} />
+              <Button
+                title={'アイテムを作成する'}
+                message={'アイテムを作成しますか？'}
+                color={'#eef0e6'}
+                backColor={'#3a526f'}
+                width={300}
+              />
+            </div>
+            {/* プレビュー用スペース */}
+            <div className="col-4">
+              <div className="position-fixed">
+                <div className="mt-5">プレビュー用スペース</div>
+                <MainButton width={414} height={500} />
+              </div>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </AuthProvider>
   )
 }
--- pages/createTreasure.tsx
+++ pages/createTreasure.tsx
@@ -2,46 +2,49 @@ import Link from 'next/link'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { MainButton } from '../components/MainButton'
 import { Header } from '../components/Header'
+import { AuthProvider } from './context/AuthContext'
 
 export default function createTreasure() {
   return (
-    <div style={{ background: '#0b1118', color: '#eef0e6' }}>
-      <Header />
-      <div className="container-sm vh-100" style={{ width: 600 }}>
-        <main className="container w-100">
-          <Link href="/">
-            <a style={{ textDecoration: 'none' }}>← go back</a>
-          </Link>
-          <h1>Create collectible</h1>
-          <p>
-            Choose “Single” if you want your collectible to be one of a kind or
-            <br />
-            “Multiple” if you want to sell one collectible multiple times
-          </p>
-          {/* メインボタンの選択 */}
-          <div className="row text-center w-100">
-            <div className="col-sm">
-              <Link href="/createSingleTreasure">
-                <a className="text-decoration-none">
-                  <MainButton width={220} height={250} title={'single'} />
-                </a>
-              </Link>
+    <AuthProvider>
+      <div style={{ background: '#0b1118', color: '#eef0e6' }}>
+        <Header />
+        <div className="container-sm vh-100" style={{ width: 600 }}>
+          <main className="container w-100">
+            <Link href="/">
+              <a style={{ textDecoration: 'none' }}>← go back</a>
+            </Link>
+            <h1>Create collectible</h1>
+            <p>
+              Choose “Single” if you want your collectible to be one of a kind or
+              <br />
+              “Multiple” if you want to sell one collectible multiple times
+            </p>
+            {/* メインボタンの選択 */}
+            <div className="row text-center w-100">
+              <div className="col-sm">
+                <Link href="/createSingleTreasure">
+                  <a className="text-decoration-none">
+                    <MainButton width={220} height={250} title={'single'} />
+                  </a>
+                </Link>
+              </div>
+              <div style={{ width: 20 }}></div>
+              <div className="col-sm">
+                <Link href="/createSingleTreasure">
+                  <a className="text-decoration-none">
+                    <MainButton width={220} height={250} title={'Multiple'} />
+                  </a>
+                </Link>
+              </div>
             </div>
-            <div style={{ width: 20 }}></div>
-            <div className="col-sm">
-              <Link href="/createSingleTreasure">
-                <a className="text-decoration-none">
-                  <MainButton width={220} height={250} title={'Multiple'} />
-                </a>
-              </Link>
-            </div>
-          </div>
-          <p>
-            We do not own your private keys and cannot access your funds
-            <br /> without your confirmation
-          </p>
-        </main>
+            <p>
+              We do not own your private keys and cannot access your funds
+              <br /> without your confirmation
+            </p>
+          </main>
+        </div>
       </div>
-    </div>
+    </AuthProvider>
   )
 }
