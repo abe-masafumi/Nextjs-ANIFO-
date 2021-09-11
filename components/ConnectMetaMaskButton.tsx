8237dc26590ff494bccfdd8da008c22c8695cd35
masafumi <masa455.wv@gmail.com>
2021-09-11 18:16:43 +0900

    update:このコンポーネントではアドレスを取得する処理をやめて、別ファイルでグローバル変数に格納することにした、なのでここではメタマスクにつなげるだけの処理

--- components/ConnectMetaMaskButton.tsx
+++ components/ConnectMetaMaskButton.tsx
@@ -1,41 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { AppProps } from 'next/dist/shared/lib/router/router';
 import Router from 'next/router'
-import { useEffect } from 'react';
 
-// const { ethers } = require("ethers");
-// console.log(ethers)
 export const ConnectMetaMaskButton = () => {
-  let accounts: []
-
   const useWindow = () => {
-    return typeof window !== "undefined"
+    return typeof window !== 'undefined'
   }
-  
-  const isMetaMaskConnected = () => {
-    return accounts && accounts.length > 0
-  }
-  
+
   const onClickConnect = async () => {
     try {
       if (useWindow()) {
-        const { ethereum } = window;
-        console.log(ethereum);
-        const newAccounts = await ethereum.request({ method: 'eth_requestAccounts' });
-        accounts = newAccounts;
-        console.log(accounts);
-        Router.push('/');
-        }
-      } catch (error) {
-        console.error(error);
-        console.error("接続がキャンセルされたかエラーが発生しました");
+        // これの赤線が気になる！
+        const { ethereum } = window
+        const newAccounts = await ethereum.request({ method: 'eth_requestAccounts' })
+        Router.push('/')
       }
-      if (isMetaMaskConnected()) {
-        alert('ログイン完了');
-      }
-    };
+    } catch (error) {
+      console.error(error)
+      console.error('接続がキャンセルされたかエラーが発生しました')
+    }
+  }
 
-      
   return (
     <>
       <div className="col-4">
