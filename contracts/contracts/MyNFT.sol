be3893ea38421eed7ce27ac57f6b90a025af8372
masafumi <masa455.wv@gmail.com>
2021-10-12 14:01:40 +0900

    add:新しいコントラクトをデプロイした

--- contracts/contracts/MyNFT.sol
+++ contracts/contracts/MyNFT.sol
@@ -6,12 +6,17 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract MyNFT is ERC721Enumerable,ERC721URIStorage, Ownable{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    
-    constructor() public ERC721("MyNFT", "NFT") {}
+    address contractAddress;
+
+    constructor(address marketplaceAddress) public ERC721("MyNFT", "NFT") {
+        contractAddress = marketplaceAddress;
+    }
 
     function mintNFT(address recipient, string memory tokenURI)
         public
@@ -21,6 +26,130 @@ contract MyNFT is ERC721URIStorage, Ownable {
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
         return newItemId;
     }
 }
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+    uint256 listingPrice = 10 ether;//ねだん
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+    }
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    event MarketItemCreated (
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price
+    );
+
+    function createMarketItem (
+    address nftContract,
+    uint256 tokenId,
+    uint256 price
+    ) public payable nonReentrant {
+        require(price > 0, "Price must be at least 1 wei");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price
+        );
+
+        IERC721(nftContract).transferFrom(msg.sender, address(this),tokenId);
+
+        emit MarketItemCreated(
+            itemId,
+            nftContract,
+            tokenId,
+            msg.sender,
+            address(0),
+            price
+        );
+    }
+
+    function createMarketSale(
+    address nftContract,
+    uint256 itemId
+    ) public payable nonReentrant {
+
+        uint price = idToMarketItem[itemId].price;
+        uint tokenId = idToMarketItem[itemId].tokenId;
+
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+        idToMarketItem[itemId].seller.transfer(msg.value);
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        _itemsSold.increment();
+        payable(owner).transfer(listingPrice);
+    }
+
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint itemCount = _itemIds.current();
+        uint unsoldItemCount = itemCount - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+        for (uint i = 0; i < itemCount; i++) {
+            if (idToMarketItem[i + 1].owner == address(0)) {
+            uint currentId = idToMarketItem[i + 1].itemId;
+            MarketItem storage currentItem = idToMarketItem[currentId];
+            items[currentIndex] = currentItem;
+            currentIndex += 1;
+            }
+        }
+        return items;
+    }
+
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+            itemCount += 1;
+           }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+            uint currentId = idToMarketItem[i + 1].itemId;
+            MarketItem storage currentItem = idToMarketItem[currentId];
+            items[currentIndex] = currentItem;
+            currentIndex += 1;
+            }
+        }
+        return items;
+    }
+}
+
--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -2,7 +2,7 @@ async function main() {
   const MyNFT = await ethers.getContractFactory("MyNFT")
 
   // Start deployment, returning a promise that resolves to a contract object
-  const myNFT = await MyNFT.deploy()
+  const myNFT = await MyNFT.deploy("0x9c8Cc3b0DeC2dbdf2ceD454eDF91695BBFd2cff2")
   console.log("デプロイしたコントラクトアドレス-->:", myNFT.address)
 }
 
