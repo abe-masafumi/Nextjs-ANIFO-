8691bc301af523faf1e76c30c6d221a7e3aad93f
masafumi <masa455.wv@gmail.com>
2021-10-19 01:46:45 +0900

    add:varcelのリンクを変更

--- pages/mintOnlyPage.js
+++ pages/mintOnlyPage.js
@@ -64,7 +64,7 @@ const Home = ({ API_URL, PUBLIC_KEY, PRIVATE_KEY }) => {
       // window.location.href = `http://localhost/myfile_lab05/%20NFTMetaData/updata.php?tokenID=${tokenId}&uniqueNumber=${uniqueNumber}`;
     } catch (error) {
       alert('実行が中断されました')
-      window.location.href = `https://nextjs-anifo.vercel.app/createSingleTreasure`;
+      window.location.href = `https://nextjs-anifo-steel.vercel.app/createSingleTreasure`;
       // window.location.href = `http://localhost:3000/createSingleTreasure`;
     }
   } 
